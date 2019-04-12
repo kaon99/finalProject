6d86652803d0877d58df8201bb9edc90a7e23ab0
Yaroslav <kaon1999@gmail.com>
2019-04-12 23:43:13 +0300

    Queries

--- src/main/java/MAIN.java
+++ src/main/java/MAIN.java
@@ -6,8 +6,6 @@ import model.entity.Student;
 
 public class MAIN {
     public static void main(String[] args) {
-        StudentDao studentDao = new StudentDaoImpl();
-        Student student = studentDao.findById(1);
-        studentDao.setSumMarks(student);
+        RatingDa
     }
 }
