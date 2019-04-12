c7f0eaf19374c70d82382df9f956f826fd0441e1
Yaroslav <kaon1999@gmail.com>
2019-04-12 23:31:19 +0300

    SpecialtyServiceImpl

--- src/main/java/model/service/SpecialtyService.java
+++ src/main/java/model/service/SpecialtyService.java
@@ -1,4 +1,15 @@
 package model.service;
 
+import model.entity.Specialty;
+
+import java.util.List;
+
 public interface SpecialtyService {
+    void create (Specialty specialty);
+
+    Specialty findById(int id);
+    List<Specialty> findAll();
+    void update(Specialty entity);
+    void delete(int id);
+
 }
--- src/main/java/model/service/impl/RatingServiceImpl.java
+++ src/main/java/model/service/impl/RatingServiceImpl.java
@@ -1,6 +1,33 @@
 package model.service.impl;
 
+import model.entity.Rating;
 import model.service.RatingService;
 
+import java.util.List;
+
 public class RatingServiceImpl implements RatingService {
+    @Override
+    public void create(Rating student) {
+
+    }
+
+    @Override
+    public Rating findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Rating> findAll() {
+        return null;
+    }
+
+    @Override
+    public void update(Rating entity) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
 }
