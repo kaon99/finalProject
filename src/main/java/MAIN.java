9725861533353330d1cee67b1b41a63d55e6fa8a
Yaroslav <kaon1999@gmail.com>
2019-04-12 23:47:28 +0300

    QueriesFix

--- src/main/java/MAIN.java
+++ src/main/java/MAIN.java
@@ -1,11 +1,16 @@
 
 import model.dao.StudentDao;
 import model.dao.daoimpl.StudentDaoImpl;
+import model.entity.Rating;
 import model.entity.Student;
+import model.service.RatingService;
+import model.service.impl.RatingServiceImpl;
 
 
 public class MAIN {
     public static void main(String[] args) {
-        RatingDa
+        RatingService ratingService = new RatingServiceImpl();
+         Rating rating  = ratingService.findById(1);
+        System.out.println(rating);
     }
 }
